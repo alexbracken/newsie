90622507d2b096645db1119cef70642b922cd9fd
alexbracken <alex@alexbracken.co>
2024-12-03 10:00:25 -0500

    Add queue creation, post creation functions

--- paperboy/helpers.py
+++ paperboy/helpers.py
@@ -1,9 +1,13 @@
 import json
 import os
 from typing import List, Set
+from dotenv import load_dotenv
+from pyfacebook import GraphAPI
+import feedparser
+from datetime import datetime, timedelta
 
 class PostTracker:
-    def __init__(self, tracking_file: str = 'posted_ids.json'):
+    def __init__(self, tracking_file: str = 'posted.json'):
         """
         Initialize a tracker to prevent duplicate posts.
         
@@ -71,11 +75,84 @@ class PostTracker:
         
         # Save to file
         self._save_posted_ids()
-        
 
 class FacebookPoster:
-#something goes here
-    def post_to_facebook(self, post):
+
+    def _create_queue(self, posts_per_day: int, day_start: int, day_end: int) -> List:
         """
-        something will go here
+        Create queue for Facebook posting
+        
+        :param posts_per_day: Number of posts/day
+        :param day_start: First post time in 24H format
+        :param day_end: Last post time in 24H format
+        
+        :return: List of slots for posts
         """
+        #  
+        if posts_per_day == 1:
+            return [day_start]
+        
+        # Calculate interval between slots
+        n = (day_end - day_start) / (posts_per_day - 1)
+        
+        slots = [day_start + i * n for i in range(posts_per_day)]
+    
+        # Ensure last slot is exactly day_end
+        slots[-1] = day_end
+        
+        return slots
+    
+    def create_posts(self, unposted_items: List[dict], page_id: str):
+        
+        # Import environment variables
+        load_dotenv()
+        
+        # GraphAPI configuration
+        fb = GraphAPI(
+            app_id = os.getenv("APP_ID"),
+            app_secret = os.getenv("APP_SECRET"),
+            access_token=os.getenv("ACCESS_TOKEN")
+        )
+        # TODO Create posting logic
+        for item in unposted_items:
+            
+            params= {"fields": "id,message,created_time,from"},
+            
+            # Test for existence of items
+            if 'title' in item:
+                title = item.title
+            link = item.link
+            summary = item.summary
+            
+            post: dict = {
+                'body': summary,
+            }
+            
+            """
+            fb.post_object(
+                page_id,
+                connection="feed",
+                data={
+                    "message": "This is a tests message by api"
+                    },
+            )
+            """
+            
+
+class FeedScraper:
+    def read_rss(self, url: str, agent: str) -> List[dict]:
+        """
+        Scrape RSS feeds
+        
+        :param ids: List of IDs to mark as posted
+        :param max_tracked_ids: Maximum number of IDs to keep track of
+        """
+        
+        url = url
+        agent = agent
+        d = feedparser.parse(
+            url,
+            request_headers = {'User-agent':agent}
+        )
+        
+        return d.entries
