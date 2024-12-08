e607770ffcd55db17198ae5e36caae08307012ad
alexbracken <alex@alexbracken.co>
2024-12-08 11:36:21 -0500

    Start class instances with config in core.py

--- paperboy/core.py
+++ paperboy/core.py
@@ -1,31 +1,40 @@
 # Import helper functions
+import logging
 from helpers import *
-from datetime import time
 
 def main():
 
     version = 0.1
 
-    # General Configuration
-    agent = 'paperboy 0.1' # User agent for HTTP requests
-    url = 'https://www.chronicle-tribune.com/search/?k=%23free&s=&f=atom&altf=mrss&ips=1080&l=100'
-    page_id = "263380803530734" # ID of target page
-    
+    # RSS Source Configuration
+    source = FeedScraper(
+        agent = 'paperboy 0.1', # User agent for HTTP requests
+        url = 'https://www.chronicle-tribune.com/search/?k=%23free&s=&f=atom&altf=mrss&ips=1080&l=100'
+    )
+
+    debug = True
     # Queue Configuration
-    posts_per_day = 6 # Number of posts per day (int)
-    day_start = 7 # First post time
-    day_end = 18 # Last post time 
+    queue = QueueManager(
+        posts_per_day = 6, # Number of posts per day (int)
+        day_start = 7, # First post time (int)
+        day_end = 18 # Last post time (int)
+        )
     
-    # Initialize feed scraper function
-    d = feedparser.parse(url, request_headers = {'User-agent':agent})
+    # Facebook Configuration
+    fb = FacebookPoster(
+        page_id = "",
+        )
+    
+    currency_limit = 2 # Max number of days after publish (int) 
     
     tracker = PostTracker()
     
     unposted_items = []
     unposted_ids = []
     
-    # Iterate through entries
-    for entry in d.entries:
+    entries = source.read_rss()
+
+    for entry in entries:
         # If entry.id is not in posted_ids.json 
         if tracker.get_unposted_ids([entry.id]):
             # Add item dict and id to respective list
@@ -35,12 +44,11 @@ def main():
     
     # Process unposted items
     if unposted_items:
-        fb = FacebookPoster()
-        slots = fb._create_queue(posts_per_day, day_start, day_end)
+        slots = queue.construct_queue()
         
         print(f"Found {len(unposted_items)} unposted items")
         
-        fb.create_posts(unposted_items, page_id)
+        fb.create_posts(unposted_items, slots)
         # fb.add_to_queue(unposted_items)
                     
         # After successful posting, mark as posted
